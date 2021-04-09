cdaeb6ceb303ebc6a9cda0809748a42dd4047172
Jasmin Elgendy <j.elgendy@ostfalia.de>
2021-04-09 14:26:06 +0200

    Die Fahrzeugapp

---
+++ app/src/main/java/com/example/dasfahrzeug/AutonomesFahren.java
@@ -0,0 +1,14 @@
+package com.example.dasfahrzeug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AutonomesFahren extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autonomes_fahren);
+    }
+}
---
+++ app/src/main/java/com/example/dasfahrzeug/Hauptbildschirm.java
@@ -0,0 +1,14 @@
+package com.example.dasfahrzeug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hauptbildschirm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hauptbildschirm);
+    }
+}
